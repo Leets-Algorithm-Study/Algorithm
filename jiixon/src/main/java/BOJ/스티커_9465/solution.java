bd5493673ddbcb89bc1d695f41be82d72269280c
jiixon <jr0107380@gmail.com>
2024-05-25 01:37:46 +0900

    BOJ 9465 Upload

--- jiixon/src/main/java/BOJ/스티커_9465/solution.java
+++ jiixon/src/main/java/BOJ/스티커_9465/solution.java
@@ -9,42 +9,43 @@ public class solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         int[] result = new int[t];
 
 
-        for(int i = 0; i<t; i++){
+        for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             int[][] sticker = new int[2][n];
 
-            //첫번째줄 스티커
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; i<n+1; j++){
-                sticker[0][j]=Integer.parseInt(st.nextToken());
-            }
+            //스티커줄 입력받기
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
 
-            //두번째줄 스티커
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; i<n+1; j++){
-                sticker[1][j]=Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                sticker[0][j] = Integer.parseInt(st1.nextToken());
+                sticker[1][j] = Integer.parseInt(st2.nextToken());
             }
 
             int[][] dp = new int[2][n];
 
-            if(n==1){ //?? 고민중
-                result[i] = Math.max(sticker[0][0],sticker[1][0]);
+            if(n==1){
+                result[i] = Math.max(sticker[0][0], sticker[1][0]);
+                continue;
             }
 
             dp[0][0] = sticker[0][0];
             dp[1][0] = sticker[1][0];
 
-            //.. 제 느낌상 이건데 ^^.. ㅠ점화식이 너무 어려워요 오늘밤까지만 한번 도전해볼게요 ㅜ 풀수 있을거같아서 경규님 코드는 안보겠슴다..
+            dp[0][1] = dp[1][0] + sticker[0][1];
+            dp[1][1] = dp[0][0] + sticker[1][1];
 
-
-
+            for (int j = 2; j < n; j++) {
+                dp[0][j] = Math.max(sticker[0][j] + dp[1][j-1], sticker[0][j]+ dp[1][j-2]);
+                dp[1][j] = Math.max(sticker[1][j] + dp[0][j-1], sticker[1][j]+ dp[0][j-2]);
+            }
+            result[i] = Math.max(dp[0][n-1],dp[1][n-1]);
         }
-
-
-
+        for(int r : result){
+            System.out.println(r);
+        }
     }
 }
